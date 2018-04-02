651a57ca7074f072b7da51641e8b8205fc125e84
Jordan Gary <JigSays@gmail.com>
2018-04-02 13:02:12 -0500

    adds screenshots for readme

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Jumbotron.css";
 
 const Jumbotron = ({ children }) =>
   <div style={{ height: 100, margin: 10, clear: 'both', padding: 10 }} className="jumbotron">
